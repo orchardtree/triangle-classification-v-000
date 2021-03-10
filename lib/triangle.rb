fe059e0fde6548e5b3f9098d541cae3af7324778
Jesse Wenger <wengerj@gmail.com>
2021-03-10 23:12:13 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,8 +9,8 @@ class Triangle
   def kind
     if @side_1 == @side_2 && @side_1 == @side_3
       :equilateral
-    elsif @side_1 == @side_2 && @side_1
-      :isosceles
+    elsif @side1 != @side_2 && @side_1 != @side_3 && @side_2 != @side_3
+      :scalene
     end
   end
 end
