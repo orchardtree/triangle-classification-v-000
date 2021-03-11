e44a6c53d5ab194957498c20a322e04895705401
Jesse Wenger <wengerj@gmail.com>
2021-03-11 00:56:54 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -5,7 +5,7 @@ class Triangle
     if side_1 == 0 || side_2 == 0 || side_3 == 0
       raise TriangleError
     elsif side_1 < 0 || side_2 < 0 || side_3 < 0
-    
+      raise TriangleError
     end
     @side_1 = side_1
     @side_2 = side_2
