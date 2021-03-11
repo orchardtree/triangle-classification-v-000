334c34fc26d03123e3d7ca04c7544ef0cc05949e
Jesse Wenger <wengerj@gmail.com>
2021-03-11 01:06:44 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,11 +9,12 @@ class Triangle
     elsif side_1 + side_2 >= side_3 || 
           side_1 + side_3 >= side_2 || 
           side_2 + side_3 >= side_1
-      raise TriangleError        
+      raise TriangleError
+    else
+      @side_1 = side_1
+      @side_2 = side_2
+      @side_3 = side_3
     end
-    @side_1 = side_1
-    @side_2 = side_2
-    @side_3 = side_3
   end
   
   def kind
