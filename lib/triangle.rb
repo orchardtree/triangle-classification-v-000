490f549c4b3e7cc9a7c58111a5c745b7a1cc718e
Jesse Wenger <wengerj@gmail.com>
2021-03-11 01:27:07 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -31,7 +31,7 @@ class Triangle
   
   class TriangleError < StandardError
     def message 
-  "Triangles with no size are illegal. Triangles with negative sides are illegal. Triangles violating triangle inequality are illegal."
+    "Triangles with no size are illegal. Triangles with negative sides are illegal. Triangles violating triangle inequality are illegal."
     end
   end
 end
