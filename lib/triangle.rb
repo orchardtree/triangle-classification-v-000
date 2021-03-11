2b80a0e3539810a54f342790ed630f1af5b3f3fa
Jesse Wenger <wengerj@gmail.com>
2021-03-11 00:19:21 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -11,8 +11,8 @@ class Triangle
     t = ""
     if @side_1 == @side_2 && @side_1 == @side_3
       t = :equilateral
-    #elsif @side1 == @side_2 || @side_1 == @side_3 || @side_2 == @side_3
-      #t = :isosceles
+    elsif @side_1 == @side_2 &&  @side_1 == @side_3 || @side_2 == @side_3
+      t = :isosceles
     elsif @side1 != @side_2 && @side_1 != @side_3 && @side_2 != @side_3
       t = :scalene
     else 
