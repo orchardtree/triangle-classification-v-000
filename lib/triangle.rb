997ad20953e556337d8700ff548dea1c220ae15b
Jesse Wenger <wengerj@gmail.com>
2021-03-11 00:56:13 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -3,13 +3,9 @@ class Triangle
   # write code here
   def initialize(side_1, side_2, side_3)
     if side_1 == 0 || side_2 == 0 || side_3 == 0
-      #begin 
-        raise TriangleError
-          #rescue TriangleError => error
-        
-        #puts error.message
-      
-      #end
+      raise TriangleError
+    elsif side_1 < 0 || side_2 < 0 || side_3 < 0
+    
     end
     @side_1 = side_1
     @side_2 = side_2
