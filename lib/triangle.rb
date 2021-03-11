4d537202f74f9437c2e075eb5f12d4ef1cd8db00
Jesse Wenger <wengerj@gmail.com>
2021-03-11 00:49:12 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -5,16 +5,17 @@ class Triangle
     @side_1 = side_1
     @side_2 = side_2
     @side_3 = side_3
-  end
-  
-  def kind
+    
     if @side_1 == 0 && @side_2 == 0 && @side_3 == 0
       begin 
         raise TriangleError
           rescue TriangleError => error
-        puts error.message
-      end
-    elsif @side_1 == @side_2 && @side_1 == @side_3
+      puts error.message
+    end
+  end
+  
+  def kind
+    if @side_1 == @side_2 && @side_1 == @side_3
       :equilateral
     elsif @side_1 == @side_2 && @side_1 != @side_3 ||
           @side_1 == @side_3 && @side_1 != @side_2 ||
