9db0cdd0871a52850545f87a1637657dc9cf36a4
Jesse Wenger <wengerj@gmail.com>
2021-03-11 01:09:37 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -6,9 +6,15 @@ class Triangle
       raise TriangleError
     elsif side_1 < 0 || side_2 < 0 || side_3 < 0
       raise TriangleError
-    elsif side_1 + side_2 !>= side_3 || 
-          side_1 + side_3 >= side_2 || 
-          side_2 + side_3 >= side_1
+    elsif side_1 + side_2 != side_3 || 
+          side_1 + side_3 != side_2 || 
+          side_2 + side_3 != side_1 ||
+          side_1 + side_2 != side_3 || 
+          side_1 + side_3 != side_2 || 
+          side_2 + side_3 != side_1 ||
+          
+          
+          
       raise TriangleError
     else
       @side_1 = side_1
